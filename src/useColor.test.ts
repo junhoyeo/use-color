23335079b05f4ed7adb309beea875598f601de20
Junho Yeo <hanaro0704@gmail.com>
2021-07-04 17:02:33 +0900

    feat: allow `useColor` to parse object from RGBA string

--- src/useColor.test.ts
+++ src/useColor.test.ts
@@ -1,6 +1,54 @@
-import { act, renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react-hooks';
 
+import { ColorInput } from './types/ColorInput';
 import { useColor } from './useColor';
+
+const toColorInput = <T extends string>(value: ColorInput<T>) => value
+
+describe('RGBA string', () => {
+  describe.each([
+    toColorInput('rgba(34,114,235,0.8)'),
+    toColorInput('rgba(34,114, 235,0.8)'),
+    toColorInput('rgba(34, 114,235, 0.80)'),
+    toColorInput('rgba(34, 114, 235, 0.8)'),
+  ])('When RGBA string is given', (colorInput) => {
+    // when
+    const { result } = renderHook(() => useColor(colorInput))
+    const color = result.current[0]!
+
+    it('Parse RGB/RGBA object', () => {
+      expect(color.rgb).toEqual({ r: 34, g: 114, b: 235 })
+      expect(color.rgba).toEqual({ r: 34, g: 114, b: 235, a: 0.8 })
+    })
+
+    it('Stringifies to Hex string', () => {
+      expect(color.strings.hex).toEqual('#2272ebcc')
+    })
+
+    it('Stringifies to RGB/RGBA string', () => {
+      expect(color.strings.rgb).toEqual('rgb(34, 114, 235)')
+      expect(color.strings.rgba).toEqual('rgba(34, 114, 235, 0.8)')
+    })
+  })
+
+  it('Each value cannot be greater than the maximum', () => {
+    const { result } = renderHook(() => useColor('rgba(0, 0, 260, 2)'))
+    const color = result.current[0]!
+
+    expect(color.rgb).toEqual({ r: 0, g: 0, b: 255 })
+    expect(color.rgba).toEqual({ r: 0, g: 0, b: 255, a: 1 })
+    expect(color.strings.hex).toEqual('#0000ff')
+  })
+
+  it('Each value cannot be less than the minimum value', () => {
+    const { result } = renderHook(() => useColor('rgba(-2, 0, 255, -1)'))
+    const color = result.current[0]!
+
+    expect(color.rgb).toEqual({ r: 0, g: 0, b: 255 })
+    expect(color.rgba).toEqual({ r: 0, g: 0, b: 255, a: 0 })
+    expect(color.strings.hex).toEqual('#0000ff00')
+  })
+})
 
 describe('When RGB object is given', () => {
   // given
--- src/useColor.ts
+++ src/useColor.ts
@@ -3,15 +3,27 @@ import { useState } from 'react';
 import { Color } from './Color';
 import { ColorInput } from './types/ColorInput';
 
+const toRgbRange = (value: number) => Math.max(Math.min(value, 255), 0)
+const toAlphaRange = (value: number) => Math.max(Math.min(value, 1), 0)
+
 export const useColor = <Str extends string>(
   colorInput: ColorInput<Str>,
 ): [Color | undefined] => {
   const [color] = useState(() => {
     if (typeof colorInput === 'string') {
       if (colorInput.startsWith('rgba')) {
-        // TODO: RgbaString
-        colorInput
-        return undefined
+        // RgbaString
+        let [red, green, blue, alpha] = colorInput
+          .replace(/^rgba?\(|\s+|\)$/g, '')
+          .split(',')
+          .map((v) => parseFloat(v))
+
+        return new Color({
+          r: toRgbRange(red),
+          g: toRgbRange(green),
+          b: toRgbRange(blue),
+          a: toAlphaRange(alpha),
+        })
       }
 
       if (colorInput.startsWith('rgb')) {

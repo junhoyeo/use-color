80a096b0a508ea28ce3ba46804de3f37a9e76197
Junho Yeo <hanaro0704@gmail.com>
2021-07-05 01:21:59 +0900

    refactor(parser): detach Hex string parsing logic

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,2 +1,3 @@
+export { parseColorFromHexString } from './Hex'
 export { parseColorFromRgbString } from './Rgb'
 export { parseColorFromRgbaString } from './Rgba'
--- src/useColor.ts
+++ src/useColor.ts
@@ -2,10 +2,11 @@ import { useState } from 'react';
 
 import { Color } from './Color';
 import { Config } from './Config';
-import { parseColorFromRgbaString, parseColorFromRgbString } from './Parser';
+import {
+  parseColorFromHexString, parseColorFromRgbaString, parseColorFromRgbString
+} from './Parser';
 import { ColorInput } from './types/ColorInput';
 import { RgbaString, RgbString } from './types/rgb';
-import { toRgbRange, toTwoDigitHex } from './utils/filters';
 
 export const useColor = <Str extends string>(
   colorInput: ColorInput<Str>,
@@ -23,45 +24,8 @@ export const useColor = <Str extends string>(
         return parseColorFromRgbString(colorInput as RgbString, config)
       }
 
-      // TODO: HexString
-      const hexString = colorInput.replace('#', '')
-      let red: string, green: string, blue: string, alpha: string | undefined
-
-      switch (hexString.length) {
-        default:
-          ;[red, green, blue] = hexString.split('').map(toTwoDigitHex)
-          break
-        case 4:
-          ;[red, green, blue, alpha] = hexString.split('').map(toTwoDigitHex)
-          break
-        case 6:
-          ;[red, green, blue] = [
-            hexString.substring(0, 2),
-            hexString.substring(2, 4),
-            hexString.substring(4, 6),
-          ].map(toTwoDigitHex)
-          break
-        case 8:
-          ;[red, green, blue, alpha] = [
-            hexString.substring(0, 2),
-            hexString.substring(2, 4),
-            hexString.substring(4, 6),
-            hexString.substring(6, 8),
-          ].map(toTwoDigitHex)
-          break
-      }
-
-      return new Color(
-        {
-          r: parseInt(red, 16),
-          g: parseInt(green, 16),
-          b: parseInt(blue, 16),
-          a: alpha //
-            ? parseFloat((parseInt(alpha, 16) / 255).toFixed(2))
-            : 1,
-        },
-        config,
-      )
+      // HexString
+      return parseColorFromHexString(colorInput, config)
     }
 
     // RgbaObject

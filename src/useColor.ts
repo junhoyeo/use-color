cab575e80e7e4d3be7f46c4e52eaa80101c7ddfd
Junho Yeo <hanaro0704@gmail.com>
2021-07-05 01:13:16 +0900

    refactor(parser): detach RGBA string parsing logic

--- src/useColor.ts
+++ src/useColor.ts
@@ -2,8 +2,10 @@ import { useState } from 'react';
 
 import { Color } from './Color';
 import { Config } from './Config';
+import { parseColorFromRgbaString } from './Parser/Rgba';
 import { ColorInput } from './types/ColorInput';
-import { toAlphaRange, toRgbRange, toTwoDigitHex } from './utils/filters';
+import { RgbaString } from './types/rgb';
+import { toRgbRange, toTwoDigitHex } from './utils/filters';
 
 export const useColor = <Str extends string>(
   colorInput: ColorInput<Str>,
@@ -13,20 +15,7 @@ export const useColor = <Str extends string>(
     if (typeof colorInput === 'string') {
       if (colorInput.startsWith('rgba')) {
         // RgbaString
-        let [red, green, blue, alpha] = colorInput
-          .replace(/^rgba?\(|\s+|\)$/g, '')
-          .split(',')
-          .map((v) => parseFloat(v))
-
-        return new Color(
-          {
-            r: toRgbRange(red),
-            g: toRgbRange(green),
-            b: toRgbRange(blue),
-            a: toAlphaRange(alpha),
-          },
-          config,
-        )
+        return parseColorFromRgbaString(colorInput as RgbaString, config)
       }
 
       if (colorInput.startsWith('rgb')) {

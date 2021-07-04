2753ec39ab4dbb95604b2d58994b7512dd1bc8af
Junho Yeo <hanaro0704@gmail.com>
2021-07-05 03:50:10 +0900

    refactor: detach `rgbaToHex` utility function to external file

--- src/Color.ts
+++ src/Color.ts
@@ -1,5 +1,6 @@
 import { Config } from './Config';
 import { RgbaObject, RgbaString, RgbObject, RgbString } from './types/rgb';
+import { rgbaToHex } from './utils/rgbaToHex';
 
 type Strings = {
   _color: Color
@@ -48,35 +49,4 @@ export class Color {
       },
     }
   }
-}
-
-export const numberToHex = (value: number) =>
-  (value | (1 << 8)).toString(16).slice(1)
-export const getCompressable = (...digits: string[]) =>
-  digits.every((value) => value[0] === value[1])
-
-type RgbaToHexOptions = {
-  compress?: boolean
-}
-export const rgbaToHex = (
-  { r: red, g: green, b: blue, a: alpha }: RgbaObject,
-  options?: RgbaToHexOptions,
-) => {
-  const redHex = numberToHex(red)
-  const greenHex = numberToHex(green)
-  const blueHex = numberToHex(blue)
-
-  let digits = [redHex, greenHex, blueHex]
-  if (typeof alpha !== 'undefined' && alpha !== 1) {
-    const hexAlpha = ((alpha * 255) | (1 << 8)).toString(16).slice(1)
-    digits.push(hexAlpha)
-  }
-
-  let hexColor = '#'
-  if (options?.compress && getCompressable(...digits)) {
-    hexColor += digits.map((value) => value[0]).join('')
-  } else {
-    hexColor += digits.join('')
-  }
-  return hexColor
 }
---
+++ src/utils/rgbaToHex.ts
@@ -0,0 +1,32 @@
+import { RgbaObject } from '../types/rgb';
+
+export const numberToHex = (value: number) =>
+  (value | (1 << 8)).toString(16).slice(1)
+export const getCompressable = (...digits: string[]) =>
+  digits.every((value) => value[0] === value[1])
+
+type RgbaToHexOptions = {
+  compress?: boolean
+}
+export const rgbaToHex = (
+  { r: red, g: green, b: blue, a: alpha }: RgbaObject,
+  options?: RgbaToHexOptions,
+) => {
+  const redHex = numberToHex(red)
+  const greenHex = numberToHex(green)
+  const blueHex = numberToHex(blue)
+
+  let digits = [redHex, greenHex, blueHex]
+  if (typeof alpha !== 'undefined' && alpha !== 1) {
+    const hexAlpha = ((alpha * 255) | (1 << 8)).toString(16).slice(1)
+    digits.push(hexAlpha)
+  }
+
+  let hexColor = '#'
+  if (options?.compress && getCompressable(...digits)) {
+    hexColor += digits.map((value) => value[0]).join('')
+  } else {
+    hexColor += digits.join('')
+  }
+  return hexColor
+}
